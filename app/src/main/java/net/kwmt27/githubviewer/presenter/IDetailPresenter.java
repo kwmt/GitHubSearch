79db728a173f5007ed395e20cbb31d58a21f3748
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-07-29 11:16:53 +0900

    clean code

--- app/src/main/java/net/kwmt27/githubviewer/presenter/IDetailPresenter.java
+++ app/src/main/java/net/kwmt27/githubviewer/presenter/IDetailPresenter.java
@@ -2,9 +2,5 @@ package net.kwmt27.githubviewer.presenter;
 
 public interface IDetailPresenter extends IBaseActivityPresenter {
 
-    String OWENER_KEY = "owner_key";
-    String REPO_KEY = "repo_key";
-
-
     String REPO_ENTITY_KET = "repo_entitiy_key";
 }
