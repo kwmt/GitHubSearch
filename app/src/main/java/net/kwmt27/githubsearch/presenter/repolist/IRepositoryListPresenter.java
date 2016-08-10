935a5d4d2f44d0a82607bbac7c9617c923585f62
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-08-10 16:13:00 +0900

    コメント削除

--- app/src/main/java/net/kwmt27/githubsearch/presenter/repolist/IRepositoryListPresenter.java
+++ app/src/main/java/net/kwmt27/githubsearch/presenter/repolist/IRepositoryListPresenter.java
@@ -2,9 +2,6 @@ package net.kwmt27.githubsearch.presenter.repolist;
 
 import net.kwmt27.githubsearch.presenter.IBaseFragmentPresenter;
 
-/**
- * Created by kwmt on 2016/08/10.
- */
 public interface IRepositoryListPresenter extends IBaseFragmentPresenter {
     void onClickReloadButton();
 
