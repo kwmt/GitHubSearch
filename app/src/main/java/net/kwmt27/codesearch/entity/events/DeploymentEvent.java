0b2a0ebf19058ecc02cfe7adea8df92c105bbcde
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-09-24 16:06:46 +0900

    add comment

--- app/src/main/java/net/kwmt27/codesearch/entity/events/DeploymentEvent.java
+++ app/src/main/java/net/kwmt27/codesearch/entity/events/DeploymentEvent.java
@@ -5,17 +5,15 @@ import com.google.gson.annotations.SerializedName;
 import net.kwmt27.codesearch.entity.EventEntity;
 import net.kwmt27.codesearch.entity.payloads.DeploymentStatusEntity;
 
+/**
+ * https://developer.github.com/v3/activity/events/types/#deploymentevent
+ *
+ * Events of this type are not visible in timelines. These events are only used to trigger hooks.
+ */
 public class DeploymentEvent extends EventEntity {
 
     @SerializedName("payload")
     private DeploymentStatusEntity mDeploymentStatusEntity;
 
-//    @Override
-//    public void action(TextView view, ClickableSpan clickableSpan) {
-//        String repoName = getRepo().getName();
-//        String action = "deployed " + repoName; //TODO
-//        view.setText(action);
-//        TextViewUtil.addLink(view, repoName, clickableSpan);
-//    }
 
 }
