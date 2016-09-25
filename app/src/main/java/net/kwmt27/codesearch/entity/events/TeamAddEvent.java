dd03e519f8d903b3a6a7e1923cfd4e29a33e2058
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-09-25 12:46:56 +0900

    add comment

--- app/src/main/java/net/kwmt27/codesearch/entity/events/TeamAddEvent.java
+++ app/src/main/java/net/kwmt27/codesearch/entity/events/TeamAddEvent.java
@@ -5,17 +5,14 @@ import com.google.gson.annotations.SerializedName;
 import net.kwmt27.codesearch.entity.EventEntity;
 import net.kwmt27.codesearch.entity.payloads.TeamAddEntity;
 
+/**
+ * https://developer.github.com/v3/activity/events/types/#teamaddevent
+ *
+ * @deprecated Events of this type are not visible in timelines. These events are only used to trigger hooks.
+ */
 public class TeamAddEvent extends EventEntity {
 
     @SerializedName("payload")
     private TeamAddEntity mTeamAddEntity;
-
-//    @Override
-//    public void action(TextView view, ClickableSpan clickableSpan) {
-//        String repoName = getRepo().getName();
-//        String action = this.getClass().getSimpleName().replace("Event", "").toLowerCase() + "ed " + repoName; // TODO
-//        view.setText(action);
-//        TextViewUtil.addLink(view, repoName, clickableSpan);
-//    }
 
 }
