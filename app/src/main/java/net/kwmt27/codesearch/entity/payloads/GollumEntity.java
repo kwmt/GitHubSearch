6c6653f1c347acd453e3af82e391ae530d3ccf4c
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-09-25 11:24:42 +0900

    support GollumEvent

--- app/src/main/java/net/kwmt27/codesearch/entity/payloads/GollumEntity.java
+++ app/src/main/java/net/kwmt27/codesearch/entity/payloads/GollumEntity.java
@@ -1,5 +1,13 @@
 package net.kwmt27.codesearch.entity.payloads;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GollumEntity {
-    // TODO
+    private List<PageEntity> mPages = new ArrayList<>();
+
+    public List<PageEntity> getPages() {
+        if(mPages == null) {return new ArrayList<>();}
+        return mPages;
+    }
 }
