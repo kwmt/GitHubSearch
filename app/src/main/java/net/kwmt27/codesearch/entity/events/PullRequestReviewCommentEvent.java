7b88ed4a8f160d5d9970d835f46bb92778dd00ae
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-02-22 21:58:52 +0900

    change pull request review comment event

--- app/src/main/java/net/kwmt27/codesearch/entity/events/PullRequestReviewCommentEvent.java
+++ app/src/main/java/net/kwmt27/codesearch/entity/events/PullRequestReviewCommentEvent.java
@@ -1,14 +1,18 @@
 package net.kwmt27.codesearch.entity.events;
 
 import android.content.Context;
+import android.graphics.Typeface;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.android.flexbox.FlexboxLayout;
 import com.google.gson.annotations.SerializedName;
 
 import net.kwmt27.codesearch.entity.EventEntity;
 import net.kwmt27.codesearch.entity.payloads.PullRequestReviewCommentEntity;
+import net.kwmt27.codesearch.util.TextViewUtil;
+import net.kwmt27.codesearch.util.ViewUtil;
 import net.kwmt27.codesearch.view.events.EventListFragment;
 
 /**
@@ -25,15 +29,36 @@ public class PullRequestReviewCommentEvent extends EventEntity {
             return newTextView(context, "data empty", false, null);
         }
 
-        FlexboxLayout flexboxLayout = newFlexboxLayout(context);
-        TextView actionTextView = newTextView(context, mPullRequestReviewCommentEntity.getAction(), true, null);
-        flexboxLayout.addView(actionTextView);
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setOnClickListener(v -> {
+            if(listener != null) {
+                listener.onLinkClick(mPullRequestReviewCommentEntity.getPullRequestEntity().getTitle(), mPullRequestReviewCommentEntity.getCommentEntity().getHtmlUrl(), getRepo());
+            }
+        });
 
-        String pullRequest = getRepo().getName() + "#" + mPullRequestReviewCommentEntity.getPullRequestEntity().getNumber();
-        TextView pullRequestTextView = newTextView(context, pullRequest, false, newOnLinkClickClickableSpan(listener, pullRequest, mPullRequestReviewCommentEntity.getPullRequestEntity().getHtmlUrl(), getRepo()));
-        flexboxLayout.addView(pullRequestTextView);
+        ViewUtil.setRippleDrawable(context, linearLayout);
 
-        return flexboxLayout;
+        // action
+        TextView actionTextView = newTextView(context, "commented " + " on pull request", true, null);
+        linearLayout.addView(actionTextView);
+
+        // title
+        TextView titleTextView = newTextView(context, mPullRequestReviewCommentEntity.getPullRequestEntity().getTitle(), false, null);
+        titleTextView.setLines(1);
+        titleTextView.setEllipsize(TextUtils.TruncateAt.END);
+        titleTextView.setTypeface(null, Typeface.BOLD);
+        linearLayout.addView(titleTextView);
+
+        // comment body
+        String body = TextViewUtil.removeNewLines(mPullRequestReviewCommentEntity.getCommentEntity().getBody());
+        TextView bodyTextView = newTextView(context, body, false, null);
+        bodyTextView.setLines(1);
+        bodyTextView.setEllipsize(TextUtils.TruncateAt.END);
+        linearLayout.addView(bodyTextView);
+
+        return linearLayout;
+
     }
 
 }
